96c4aabb584c3d91af2149b52ab0a05c178c5e13
Decent-boy <aatifiqbal836@gmail.com>
2024-11-26 21:53:28 +0500

    Add files via upload

---
+++ question.py
@@ -0,0 +1,43 @@
+# # question 1
+"""el=[]
+n=int(input("enter limit of value: "))
+for i in range(1,n+1):
+    user=input("enter values: ")
+    el.append(user)
+def show(el):
+    print(f"the length of list is\n{len(el)}")
+show(el)
+print(el)"""
+
+# question 2
+"""find sum"""
+# li=[]
+# n=int(input("enter limit of list: "))
+# for i in range(1,n+1):
+#     user=input("Enter values: ")
+#     li.append(user)
+# def show(li):
+#     for j in li:
+#         print(j,end=" ")
+# show(li)
+# print("\n",li)
+# question 3
+"""find factorial"""
+# def show(n):
+#     j=1
+#     for i in range(1,n+1):
+#         j*=i
+#     print(f"factorial is{j}")
+        
+# n=int(input("enter value: "))
+# show(n)
+# question 3
+"""find sum"""
+# def show(n):
+#     if (n==1 or n==0):
+#         return 1
+#     else:
+#         return n+show(n-1)
+# n=int(input("enter valus: "))
+# show(n)
+# print(f"the sum is {show(n)}")
